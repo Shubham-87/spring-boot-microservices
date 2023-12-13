bbc465dbdd80acfc4449f8ad34f8b93ad4f38dd0
Shubham-87 <35127809+Shubham-87@users.noreply.github.com>
2023-12-13 11:44:51 +0530

    Update InventoryController.java

--- inventory-service/src/main/java/com/programmingtechie/inventoryservice/controller/InventoryController.java
+++ inventory-service/src/main/java/com/programmingtechie/inventoryservice/controller/InventoryController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+//This is Controller
 @RestController
 @RequestMapping("/api/inventory")
 @RequiredArgsConstructor
